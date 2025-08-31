f7e7200fcd4f6d42b0cf475dd88831d3f8122e23
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-08-31 23:54:54 +0545

    Add conditional statement implementations: student pass/fail, leap year, temperature check, number sign, calculator, and day of the week

---
+++ Java Basics/Conditional Statement/Questions/question1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class question1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number: ");
+        int num = sc.nextInt();
+        if (num>0){
+            System.out.println("Positive");
+        }else if(num<0){
+            System.out.println("Negative");
+        }else{
+            System.out.println("Zero");
+        }
+    }
+}
---
+++ Java Basics/Conditional Statement/calculator.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Please enter +, -, *, /, %: ");
+        char ch = sc.next().charAt(0);
+
+        switch (ch) {
+            case '+':
+                System.out.println(a + b);
+                break;
+
+            case '-':
+                System.out.println(a - b);
+                break;
+
+            case '*':
+                System.out.println(a * b);
+                break;
+
+            case '/':
+                System.out.println(a / b);
+                break;
+
+            case '%':
+                System.out.println(a % b);
+                break;
+
+            default:
+                System.out.println("Invalid operator");
+                break;
+        }
+    }
+}
